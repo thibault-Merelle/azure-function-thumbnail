da93f4dcceebc55ffce0b0ff54b073981cf44259
Thibault_Merelle <merellethibault@gmail.com>
2021-03-31 17:36:26 +0200

    init jimp && packages

---
+++ index.js
@@ -0,0 +1,31 @@
+var Jimp = require("jimp");
+
+module.exports = (context, myBlob) => {
+    context.log("blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+    // Read image with Jimp
+    Jimp.read(myBlob).then((image) => {
+        // Manipulate image
+        image
+            .clone()
+            .resize(200, Jimp.AUTO)
+            //.greyscale()
+            .quality(50) // set the quality for JPEGs
+            .getBuffer(Jimp.MIME_JPEG, (error, stream) => {
+
+                // Check for errors
+                if (error) {
+                    context.log(`There was an error processing the image.`);
+                    context.done(error);
+                }
+                else {
+                    context.log(`Successfully processed the image`);
+                    // Bind the stream to the output binding to create a new blob
+                    context.done(null, stream);
+
+                }
+
+            });
+
+    });
+
+};
